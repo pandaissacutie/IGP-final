efad9423ece1f5fe1e4ff8d221bc91fa79438e36
natalie111b <123928831+natalie111b@users.noreply.github.com>
2023-02-01 20:39:30 +0800

    Update the changes for code and comments

--- overheads.py
+++ overheads.py
@@ -14,7 +14,7 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
 
 # Creating an empty list to store name of the overheads and the overhead's percentage 
     overheads = [] 
-# Append name of the overheadds and the overhead's percentage as a list back to each empty list
+# Append name of the overheads and the overhead's percentage as a list back to each empty list
     for row in reader:
         overheads.append([row[0], row[1]])
 
@@ -28,16 +28,19 @@ def overheads_function():
     percentage_of_overheads= []
     # Creating an empty list to store the final statement 
     statement= []
+    
     # Creating a for loop to loop the items in the overheads list as data
     for data in overheads:
     # Append the percentage of overheads into the percentage_of_overheads list
         percentage_of_overheads.append(float(data[1]))
     # Using max function to identify the highest overhead's percentage and assigning it to a variable called highest_overheads_percentage
         highest_overheads_percentage = max(percentage_of_overheads)
+        
     # If the highest overheads percentage is the same as the percentage of the item in data, the name of the highest overheads will be identified 
         if float(data[1]) == highest_overheads_percentage:
     # A highest overheads statement will be appended into the statement list
            statement.append(f'[HIGHEST OVERHEADS] {data[0]}: {highest_overheads_percentage}%')
+        
     # The function will return statement at the end of all the loops
     return statement 
 
