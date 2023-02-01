11dcd4cabc31b25ed8af24ca3f54959b52e211b7
elrina22 <122434045+elrina22@users.noreply.github.com>
2023-02-01 20:21:12 +0800

    Updating comments and code

--- profit_loss.py
+++ profit_loss.py
@@ -28,27 +28,33 @@ def profitloss_function():
     '''
     # Assigning True to a variable called everyday_surplus to check if everyday surplus is true or false
     everyday_surplus= True
+    
     # Creating an empty list to store the final statement 
     statement= []
-     # A for loop is created to loop the second item's position to the last items's position of the net profit list 
+    
+     # A for loop is created to loop the position of each nested list of the cash on hand list from a range of 1 to len(Cash_on_hand)
     for index in range(1, len(netprofit)):
     # The previous net profit is extracted from the netprofit list using the current loop's index-1 and converted to integer 
         previous_netprofit= int(netprofit[index-1][1])
     # The current net profit is extracted from the netprofit list and converted into an integer 
         current_netprofit= int(netprofit[index][1])
-    # The difference between current net profit and previous net profit is calculated by substracting them with each other and assigned to a variable called diff
+    # To calculate the variable called diff, previous cash on hand is deducted from current cash on hand
         diff= current_netprofit - previous_netprofit
-    # If diff is less than zero, meaning that there is deficit on that day
+        
+    # If diff is less than zero, there is deficit a on that day
         if diff < 0:
-    # False will be assigned to everyday_surplus
+    # False will be assigned to everyday_surplus if diff < 0
             everyday_surplus = False
+        
     # A profit deficit statement with the day and amount of profit deficit will be appended into the statement list
             statement.append(f"[PROFIT DEFICIT]:DAY {int(netprofit[index][0])}, AMOUNT: USD {-(diff)}")
-    # If everyday_surplus is True which means none of the loop satisfied the condition of diff < 0 
+        
+    # If everyday_surplus is True, it would mean that none of the loop satisfied the condition of diff < 0 
     if everyday_surplus == True:
     # A net profit surplus statement will be appended into the statement list
         statement.append(f"[NET PROFIT SURPLUS] NET PROFIT IN EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
-    # the items in the statement list will be returned after the end of all the for loops 
+        
+    # The items in the statement list will be returned after the end of all the for loops 
     return statement
 
 # The function is assigned to a variable called summary
@@ -57,11 +63,13 @@ summary = profitloss_function()
 
 # Import Path method from Path Library
 from pathlib import Path
+
 # Assigning the summary report text file path to a variable called file path
 file_path = Path.cwd()/"summary_report.txt"
 
 # Open the file in file_path using .open and "a" to append text in the text file
 with file_path.open(mode="a", encoding= "UTF-8") as file:
+    
 # A for loop is created to ensure that each item in the statement list is printed on a different line
     for item in summary:
         file.write(f'{item}\n')
